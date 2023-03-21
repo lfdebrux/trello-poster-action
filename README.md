914497b319a8ef4e0c1d03789073dfec6fe892fa
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-03-21 13:57:29 +0000

    Add Trello Poster JavaScript action

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # Trello Poster Action
 
-A GitHub action that automatically adds pull request links to Trello cards.
+A GitHub action that attaches GitHub links to Trello cards.
+
+## Trello API key and token
+
+The Trello API requires that the calls provide both an API key and token. See the [Trello API documentation](https://developer.atlassian.com/cloud/trello/guides/rest-api/api-introduction/#managing-your-api-key) for more details.
+
+## Inputs
+
+### `comment-body`
+
+**Required** The body text of the pull request or issue comment that may contain the link to Trello.
+
+### `github-link`
+
+**Required** The URL of the GitHub page you want to attach to the Trello card mentioned in `comment-body`.
+
+### `trello-api-key`
+
+**Required** A Trello API key for the application.
+
+### `trello-api-token`
+
+**Required** A Trello API token for the Trello user.
 
 ## Acknowledgements
 
---
+++ index.js
@@ -0,0 +1,42 @@
+import * as core from '@actions/core'
+import { HttpClient } from '@actions/http-client'
+
+const trelloCardUrlRe = 'https://trello.com/c/(\\w{8})'
+
+try {
+  const commentBody = core.getInput('comment-body')
+  const githubUrl = core.getInput('github-url')
+  const trelloApiToken = core.getInput('trello-api-token')
+  core.setSecret(trelloApiToken)
+  const trelloApiKey = core.getInput('trello-api-key')
+  core.setSecret(trelloApiKey)
+
+  function trelloApiUrl(endpoint) {
+    const url = new URL(`https://api.trello.com/1${endpoint}`)
+    url.searchParams.set('key', trelloApiKey)
+    url.searchParams.set('token', trelloApiToken)
+    return url.toString()
+  }
+
+  const match = pullRequestBody?.match(trelloCardUrlRe)
+  if (match) {
+    const http = new HttpClient('trello-poster-action')
+    const trelloCardUrl = match[0]
+    const trelloCardId = match[1]
+
+    const { result: trelloCardAttachments } = await http.getJson(
+      trelloApiUrl(`/cards/${trelloCardId}/attachments?fields=url`)
+    )
+    if (trelloCardAttachments.find((attachment) => attachment.url.startsWith(pullRequestUrl))) {
+      // nothing to do, break out
+      core.info(`Trello card ${trelloCardUrl} already has GitHub link ${githubUrl} attached`)
+    } else {
+      await http.postJson(
+        trelloApiUrl(`/cards/${trelloCardId}/attachments?url=${pullRequestUrl}`)
+      )
+      core.info(`Successfully attached GitHub link ${githubUrl} to Trello card ${trelloCardUrl}`)
+    }
+  }
+} catch (error) {
+  core.setFailed(error.message)
+}
